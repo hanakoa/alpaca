071518b546cf7e9bfc43cfa78cf911641f5144d4
Kevin Chen <kevinmichaelchen@gmail.com>
2018-04-12 20:45:16 -0400

    Fix password reset tests

--- password-reset/main_test.go
+++ password-reset/main_test.go
@@ -28,17 +28,15 @@ func TestMain(m *testing.M) {
 }
 
 func TestSendPasswordReset(t *testing.T) {
-	if tx, err := db.Begin(); err != nil {
-		panic(err)
-	} else {
+	ClearTable(db)
+	Convey("Given a user with an email and username", t, WithTransaction(db, func(tx *sql.Tx) {
+		tx, err := db.Begin()
+		So(err, ShouldBeNil)
 		tx.Exec(`INSERT INTO person (id, username) VALUES (1, 'kevin_chen')`)
 		tx.Exec(`INSERT INTO email_address (id, person_id, email_address, confirmed, is_primary) VALUES (1, 1, 'kevin.chen.bulk@gmail.com', TRUE, TRUE)`)
-		if err := tx.Commit(); err != nil {
-			panic(err)
-		}
-	}
+		err = tx.Commit()
+		So(err, ShouldBeNil)
 
-	Convey("Given a user with an email and username", t, WithTransaction(db, func(tx *sql.Tx) {
 		Convey("Should return no options and state that an email has been sent", func() {
 			So(sendCodeRequest("kevin_chen"), ShouldEqual, `"Reset request submitted."`)
 			So(sendCodeRequest("kevin.chen.bulk@gmail.com"), ShouldEqual, `"Reset request submitted."`)
@@ -47,18 +45,16 @@ func TestSendPasswordReset(t *testing.T) {
 }
 
 func TestSendPasswordResetPhoneNumber(t *testing.T) {
-	if tx, err := db.Begin(); err != nil {
-		panic(err)
-	} else {
+	ClearTable(db)
+	Convey("Given a user with an email, username, and phone number", t, WithTransaction(db, func(tx *sql.Tx) {
+		tx, err := db.Begin()
+		So(err, ShouldBeNil)
 		tx.Exec(`INSERT INTO person (id, username) VALUES (1, 'kevin_chen')`)
 		tx.Exec(`INSERT INTO email_address (id, person_id, email_address, confirmed, is_primary) VALUES (1, 1, 'kevin.chen.bulk@gmail.com', TRUE, TRUE)`)
 		tx.Exec(`INSERT INTO phone_number (id, person_id, phone_number, confirmed) VALUES (1, 1, '5555555555', TRUE)`)
-		if err := tx.Commit(); err != nil {
-			panic(err)
-		}
-	}
+		err = tx.Commit()
+		So(err, ShouldBeNil)
 
-	Convey("Given a user with an email, username, and phone number", t, WithTransaction(db, func(tx *sql.Tx) {
 		Convey("Options should include emails and phone", func() {
 			res := sendCodeRequest("kevin_chen")
 			So(res, ShouldEqual, `{"phone_numbers":[{"id":1,"phone_number":"55","person_id":1}],"email_addresses":[{"id":1,"email_address":"ke*************@g****.com","person_id":1}]}`)
@@ -78,14 +74,10 @@ func TestSendPasswordResetPhoneNumber(t *testing.T) {
 func sendCodeRequest(account string) string {
 	sendCodeRequest := &services.SendCodeRequest{Account: account}
 	b, err := json.Marshal(sendCodeRequest)
-	if err != nil {
-		panic(err)
-	}
+	So(err, ShouldBeNil)
 
 	req, err := http.NewRequest("POST", "/password-reset", bytes.NewBuffer(b))
-	if err != nil {
-		panic(err)
-	}
+	So(err, ShouldBeNil)
 	res := ExecuteRequest(req)
 	return res.Body.String()
 }
